79b5200ea9cbdf506338b0415185bdc89fabe943
Yurbanis <yurbanisbriceno@gmail.com>
2023-02-21 23:48:11 +0000

    formulario js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("myAnchor").addEventListener("click", function(event) {
+  event.preventDefault();
+});
